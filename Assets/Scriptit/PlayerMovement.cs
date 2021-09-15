f2cd2de2f1a0efcd9998295fdb8e3df9b8d3f6ec
tlantunen <lantunen00@gmail.com>
2021-09-15 16:57:24 +0300

    kokeillaan toimiiko

--- Assets/Scriptit/PlayerMovement.cs
+++ Assets/Scriptit/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] private Transform debugHitPointTransform;
+    [SerializeField] private Transform hookshotTransform;
+
     public CharacterController controller;
 
     public float speed = 12f;
@@ -15,15 +18,56 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    private Camera playerCamera;
+    private State state;
+    private Vector3 hookshotPosition;
+    private Vector3 characterVelocityMomentum;
+    private float hookshotSize;
+
     Vector3 velocity;
     bool isGrounded;
+
+    private enum State
+    {
+        Normal,
+        HookshotThrown,
+        HookshotFlyingPlayer
+    }
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = transform.Find("FPSCamera").GetComponent<Camera>();
+        state = State.Normal;
+        hookshotTransform.gameObject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        switch (state)
+        {
+            default:
+            case State.Normal:
 
+                HandleHookshotStart();
+                HandleMovement();
+                break;
+            case State.HookshotThrown:
+                HandleHookshotThrow();
+                HandleMovement();
+                break;
+            case State.HookshotFlyingPlayer:
+                HandleHookshotMovement();
+                break;
+        }
+        
+    }
+
+    private void HandleMovement()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -35,13 +79,107 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (TestInputJump() && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
-
+        velocity += characterVelocityMomentum;
         controller.Move(velocity * Time.deltaTime);
+        if (characterVelocityMomentum.magnitude >= 0f)
+        {
+            float momentumDrag = 3f;
+            velocity -= characterVelocityMomentum;
+            characterVelocityMomentum -= characterVelocityMomentum * momentumDrag * Time.deltaTime;
+            if (characterVelocityMomentum.magnitude < .0f)
+            {
+                characterVelocityMomentum = Vector3.zero;
+            }
+        }
     }
+
+    private void ResetGravityEffect()
+    {
+        velocity.y = -2f;
+    }
+
+    private void HandleHookshotStart() {
+        if (TestInputDownHookshot())
+        {
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit raycastHit)){
+                debugHitPointTransform.position = raycastHit.point;
+                hookshotPosition = raycastHit.point;
+                hookshotSize = 0f;
+                hookshotTransform.gameObject.SetActive(true);
+                hookshotTransform.localScale = Vector3.zero;
+                state = State.HookshotThrown;
+            }
+        }
+    }
+
+    private void HandleHookshotThrow()
+    {
+        hookshotTransform.LookAt(hookshotPosition);
+
+        float hookshotThrowSpeed = 130f;
+        hookshotSize += hookshotThrowSpeed * Time.deltaTime;
+        hookshotTransform.localScale = new Vector3(1, 1, hookshotSize);
+
+        if (hookshotSize >= Vector3.Distance(transform.position, hookshotPosition))
+        {
+            state = State.HookshotFlyingPlayer;
+        }
+    }
+
+    private void HandleHookshotMovement()
+    {
+        hookshotTransform.LookAt(hookshotPosition);
+        Vector3 hookshotDir = (hookshotPosition - transform.position).normalized;
+
+        float hookshotSpeedMin = 20f;
+        float hookshotSpeedMax = 40f;
+        float hookshotSpeed = Mathf.Clamp(Vector3.Distance(transform.position, hookshotPosition), hookshotSpeedMin, hookshotSpeedMax);
+        float hookshotSpeedMultiplier = 2f;
+
+        controller.Move(hookshotDir * hookshotSpeed * hookshotSpeedMultiplier * Time.deltaTime);
+
+
+        float reachedHookshotPositionDistance = 3f;
+        if (Vector3.Distance(transform.position, hookshotPosition) < reachedHookshotPositionDistance)
+        {
+            state = State.Normal;
+            ResetGravityEffect();
+            hookshotTransform.gameObject.SetActive(false);
+        }
+
+        if (TestInputDownHookshot())
+        {
+            state = State.Normal;
+            ResetGravityEffect();
+            hookshotTransform.gameObject.SetActive(false);
+        }
+
+        if (TestInputJump())
+        {
+            float momentumExtraSpeed = 3f;
+            characterVelocityMomentum = hookshotDir * hookshotSpeed * momentumExtraSpeed;
+            float JumpSpeed = 30f;
+            characterVelocityMomentum += Vector3.up * JumpSpeed;
+            state = State.Normal;
+            ResetGravityEffect();
+            hookshotTransform.gameObject.SetActive(false);
+        }
+    }
+
+    private bool TestInputDownHookshot()
+    {
+        return Input.GetKeyDown(KeyCode.E);
+    }
+
+    private bool TestInputJump()
+    {
+        return Input.GetButtonDown("Jump");
+    }
+
 }
